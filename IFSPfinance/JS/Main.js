70735619682f7fdb3731ce4ef009f6ffc57418de
Felpzcr7 <felipinholeandro0@gmail.com>
2025-09-09 22:27:01 -0300

    tendi nada

--- IFSPfinance/JS/Main.js
+++ IFSPfinance/JS/Main.js
@@ -1,9 +1,9 @@
-function openmodal () {
-    const modal = document.querySelector(".modal")
+function openmodal (modalId) {
+    const modal = document.querySelector(modalId)
     modal.style.display = "flex"
 }
-function closemodal () {
-    const modal = document.querySelector(".modal")
+function closemodal (modalId) {
+    const modal = document.querySelector(modalId)
     modal.style.display = "none"
 }
 function addticker (event) {
@@ -14,7 +14,7 @@ function addticker (event) {
     const Ativos= event.target.Ativos.value
     const valor= event.target.valor.value
     const card = `
-    <div class="carteira">
+    <div class="carteira"  onmouseenter="showbuttons(event)" onmouseleave="hidebuttons(event)">
                         <div class="ticker">
                             <p>${ticker}</p>
                             <P>${bolsa}</P>
@@ -46,6 +46,97 @@ function addticker (event) {
                             <p>U$${valor * Ativos}</p>
 
                         </div>
+                        <div class="buttons">
+                            <button type="button" onclick=""> editar</button>
+                            <button type="button" onclick="deletecard(event)"> excluir</button>
+                    </div>
+    
+    
+    `
+    const cards = document.querySelector("#cards")
+    cards.innerHTML += card
+    closemodal('#add')
+
+
+}
+function showbuttons (event){
+    const buttons = event.target.querySelector(".buttons")
+    buttons.style.display = "flex"
+}
+function hidebuttons (event){
+    const buttons = event.target.querySelector(".buttons")
+    buttons.style.display = "none"
+}
+function deletecard(event){
+    const carteira = event.target.closest(".carteira")
+    carteira.remove()
+}
+function openeditcard(event){
+    ope
+    
+    const carteira= event.target.closest(".carteira")
+
+    const ticker= carteira.querySelector('espacador p').innertext
+    const inputeditticker = document.getElementbyId(editticker)
+    inputeditticker.value = ticker
+
+    const valor= carteira.querySelector('meio p p').innertext
+    const inputeditvalor = document.getElementbyId(editvalor)
+    inputeditvalor.value = valor
+
+    const bolsa= carteira.querySelector('ticker h2').innertext
+    const selecteditbolsa = document.getElementbyId(editbolsa)
+    selecteditbolsa.value = bolsa
+    const option = selecteditbolsa.querySelector('option[value=$bolsa]')
+    option.setAttribute
+
+
+   
+}
+
+function editticker (event) {
+    event.preventDefault()
+
+    const ticker= event.target.ticker.value
+    const bolsa= event.target.bolsa.value
+    const Ativos= event.target.Ativos.value
+    const valor= event.target.valor.value
+    const card = `
+    <div class="carteira" id="${ticker}" onmouseenter="showbuttons(event)" onmouseleave="hidebuttons(event)">
+                        <div class="ticker">
+                            <p>${ticker}</p>
+                            <P>${bolsa}</P>
+                        </div>
+                        <div class="espacador">
+                            <p>Quantidade:</p>
+                            <p>${Ativos}</p>
+
+                        </div>
+
+                        <div class="espacador">
+
+
+                        </div>
+
+                        <div class="meio">
+                            <p> U$${valor} </p>
+
+                        </div>
+
+                        <div class="variacaop">
+
+                            <img src="img/setaverde.png" alt="">
+                            <p>+U$19,00</p>
+
+                        </div>
+                        <div class="espacador">
+                            <p>Posição:</p>
+                            <p>U$${valor * Ativos}</p>
+
+                        </div>
+                        <div class="buttons">
+                            <button type="button" onclick=""> editar</button>
+                            <button type="button" onclick="deletecard(event)"> excluir</button>
                     </div>
     
     
